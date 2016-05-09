25387800ee38198e30f4e9795928ced995bbfb42
julien.levillain <jlevillain@excilys.com>
2016-05-09 12:39:06 +0200

    Add directive tableHeader for link in table

---
+++ app/scripts/directives/table-header.directive.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name formationAngularApp.directive:tableHeader
+   * @description
+   * # tableHeader
+   * Directive of the formationAngularApp
+   */
+
+  angular.module('formationAngularApp')
+    .directive('tableHeader', tableHeader);
+
+    function tableHeader() {
+      return {
+        restrict:'E',
+        replace:true,
+        transclude:true,
+        scope:{
+          orderBy:"=",
+          main:"="
+        },
+        templateUrl:'../../views/directives/table-header.html',
+        link:link
+      }
+
+      function link(scope, element, attrs) {
+        console.log(scope.orderBy);
+      }
+    }
+})();
